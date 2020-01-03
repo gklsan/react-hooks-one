269266f2909a7663f593f98d8aa22fb9951cdc70
Gokul <pgokulmca@gmail.com>
2020-01-03 22:27:16 +0530

    react router added and welcome dashboard and form components are added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './index.css';
+import WelcomePage from "./components/WelcomePage";
+import Dashboard from "./components/Dashboard";
+import Form from "./components/Form";
 
-ReactDOM.render(<h1> Hai </h1>, document.getElementById('root'));
+const Index = () => {
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/form">
+            <Form />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <WelcomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
