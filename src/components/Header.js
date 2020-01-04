6d032de59b41cd514af8672f9bdea8c474cdaf68
Gokul <pgokulmca@gmail.com>
2020-01-04 23:26:03 +0530

    navbar has been added with react routers

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import Form from "./Form";
+import Dashboard from "./Dashboard";
+import WelcomePage from "./WelcomePage";
+
+const Header = () => {
+  return(
+    <Router>
+      <div>
+        <div className="header">
+          <h1 className="logo">Gklsan</h1>
+          <ul className="nav">
+            <li className="navlink">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="navlink">
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="navlink">
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+          <div className="hamburger">
+            <i className="fas fa-bars"></i>
+          </div>
+        </div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/form">
+            <Form />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <WelcomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Header from "./components/Header";
 import './index.css';
-import WelcomePage from "./components/WelcomePage";
-import Dashboard from "./components/Dashboard";
-import Form from "./components/Form";
 
-const Index = () => {
-  return(
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/form">Form</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/form">
-            <Form />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/">
-            <WelcomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  )
-}
-
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
