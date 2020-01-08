608ec6b3a1a76fab06c60a7b7c080fa2c1a00433
Gokul <pgokulmca@gmail.com>
2020-01-09 01:46:58 +0530

    final version

--- src/components/Dashboard/CardContainer.js
+++ src/components/Dashboard/CardContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CardHeader = ({image, name}) => (
   <div className={"card-logo"}>
@@ -6,19 +6,59 @@ const CardHeader = ({image, name}) => (
   </div>
 );
 
-const CardBody = ({id, name, bloodgroup, phone}) => (
-  <div>
-    <p>ID: {id}</p>
-    <p>Name: {name}</p>
-    <p>Bloodgroup: {bloodgroup}</p>
-    <p>Phone: {phone}</p>
-  </div>
-);
+const CardBody = (props) => (props.edit ? <EditBoard {...props} /> : <DisplayBoard {...props} />);
 
-const CardFooter = ({ deleteEmp, id }) => {
+const EditBoard = ({id, name, bloodgroup, phone, onFormSubmit, setEdit}) => {
+  const [data, setData] = useState({id, name, bloodgroup, phone});
+
+  const onFieldChange =(event) => {
+    const {name, value} = event.target;
+    setData({...data, [name]: value});
+  };
+
+  return(
+    <form onSubmit={(event) =>{
+      event.preventDefault();
+      onFormSubmit(event, data);
+      setEdit(false)
+    }}>
+      <input type={'hidden'} value={id} />
+      <label> Name
+        <input name={"name"} onChange={onFieldChange} required value={data.name}/>
+      </label>
+
+      <label> Blood Group
+        <input name={"bloodgroup"} onChange={onFieldChange} required value={data.bloodgroup} />
+      </label>
+
+      <label> phone
+        <input name={"phone"} onChange={onFieldChange} required value={data.phone}/>
+      </label>
+      <button>
+        Save
+      </button>
+    </form>
+  )
+}
+
+const DisplayBoard = ({id, name, bloodgroup, phone}) => {
+  return(
+    <div>
+      <p>ID: {id}</p>
+      <p>Name: {name}</p>
+      <p>Bloodgroup: {bloodgroup}</p>
+      <p>Phone: {phone}</p>
+    </div>
+  )
+}
+
+const CardFooter = ({ deleteEmp, id, setEdit, edit }) => {
+  if(edit) return <> </>;
   return(
     <>
-      <button>Edit</button>
+      <button onClick={() => setEdit(true)}>
+        Edit
+      </button>
       <button onClick={() => deleteEmp(id)}>
         Remove
       </button>
@@ -27,11 +67,14 @@ const CardFooter = ({ deleteEmp, id }) => {
 }
 
 const CardContainer = (props) => {
+  const initialVal = props.id ? false : true
+  const [edit, setEdit] = useState(initialVal);
+
   return(
     <div className={"card-item"}>
       <CardHeader {...props}/>
-      <CardBody {...props} />
-      <CardFooter {...props} />
+      <CardBody {...props} edit={edit} setEdit={setEdit} />
+      <CardFooter {...props} setEdit={setEdit} edit={edit} />
     </div>
   )
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,23 +1,38 @@
 import React, { useContext, useState } from "react";
 import CardContainer from "./CardContainer";
 import EmpContext from "../../contexts/EmpContext";
+import UUID from "uuid";
 
 const Dashboard = () => {
   const empContext = useContext(EmpContext);
   const empImages = empContext.empImages;
-  const [empDetails, setEmpDetails] = useState(empContext.empDetails);
+  const [empDetails, setEmpDetails] = useState([{id: undefined}, ...empContext.empDetails]);
 
   const deleteEmp = (id) => {
     const updatedEmps = empDetails.filter(emp => emp.id !== id);
     setEmpDetails(updatedEmps)
   };
 
+  const onFormSubmit = (event, data) => {
+    if(data.id) {
+      const updatedData = empDetails.map(emp => {
+        if (emp.id === data.id) return {...emp, ...data};
+        return emp
+      });
+      setEmpDetails(updatedData);
+    } else {
+      const newData = {...data, id: UUID()};
+      setEmpDetails([newData, ...empDetails]);
+    }
+  };
+
   return(
     <div className={"cards"}>
       {
-        empDetails.map(emp => {
-          const img = (empImages[emp.id] && empImages[emp.id].thumbnailUrl) || "https://via.placeholder.com/150/65ad4f";
-          return <CardContainer key={emp.id} image={img} deleteEmp={deleteEmp} {...emp}/>
+        empDetails.map((emp, idx) => {
+          const { id = undefined } = emp;
+          const img = empImages[idx].thumbnailUrl || "https://via.placeholder.com/150/65ad4f";
+          return <CardContainer key={idx} image={img} onFormSubmit={onFormSubmit} deleteEmp={deleteEmp} {...emp}/>
       })}
     </div>
   )
